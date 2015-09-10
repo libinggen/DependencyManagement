3046d8fe97635538aab1a69a3f7ea2513da57c97
libinggen <cralison@163.com>
2015-09-10 14:25:24 +0800

    Coding
    Coding

--- DependencyManagement/Post.m
+++ DependencyManagement/Post.m
@@ -7,36 +7,9 @@
 //
 
 #import "Post.h"
+#include <objc/runtime.h>
 
 @implementation Post
-- (instancetype)initWithDict:(NSDictionary *)dict
-{
-    if (self = [super init]) {
-        
-        [self setValuesForKeysWithDictionary:dict];
-        self.member = [Member objectWithDict:dict[@"member"]];
-        self.node = [Node objectWithDict:dict[@"node"]];
-        
-    }
-    return self;
-}
-
-- (id)initWithCoder:(NSCoder *)decoder
-{
-    if (self = [super init]) {
-//        [self decode:decoder];
-        self.title = [decoder decodeObjectForKey:@"title"];
-        self.member = [decoder decodeObjectForKey:@"member"];
-    }
-    return self;
-}
-
-- (void)encodeWithCoder:(NSCoder *)encoder
-{
-//    [self encode:encoder];
-    [encoder encodeObject:self.title forKey:@"title"];
-    [encoder encodeObject:self.member forKey:@"member"];
-}
 
 + (instancetype)objectWithDict:(NSDictionary *)dict
 {
@@ -54,5 +27,67 @@
         return objectArray;
     }
     return nil;
+}
+
+- (instancetype)initWithDict:(NSDictionary *)dict
+{
+    if (self = [super init]) {
+        
+        [self setValuesForKeysWithDictionary:dict];
+        self.member = [Member objectWithDict:dict[@"member"]];
+        self.node = [Node objectWithDict:dict[@"node"]];
+        
+    }
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)decoder
+{
+    if (self = [super init]) {
+        [self decode:decoder];
+    }
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)encoder
+{
+    [self encode:encoder];
+}
+
+- (void) decode:(NSCoder *)decoder
+{
+    unsigned int numIvars = 0;
+    Ivar *ivars = class_copyIvarList([self class], &numIvars);
+    for (int i = 0; i < numIvars; i++) {
+        Ivar thisIvar = ivars[i];
+        NSString *key = [NSString stringWithUTF8String:ivar_getName(thisIvar)];
+        [self decodeWithCoder:decoder key:key];
+    }
+}
+
+- (void) encode:(NSCoder *)encoder
+{
+    unsigned int numIvars = 0;
+    Ivar *ivars = class_copyIvarList([self class], &numIvars);
+    for (int i = 0; i < numIvars; i++) {
+        Ivar thisIvar = ivars[i];
+        NSString *key = [NSString stringWithUTF8String:ivar_getName(thisIvar)];
+        [self encodeWithCoder:encoder key:key];
+    }
+}
+
+-(void)decodeWithCoder:(NSCoder *)decoder key:(NSString *)key
+{
+    if(key){
+        [self setValue:[decoder decodeObjectForKey:key] forKey:key];
+    }
+}
+
+-(void)encodeWithCoder:(NSCoder *)encoder key:(NSString *)key
+{
+    id value = [self valueForKey:key];
+    if (value){
+        [encoder encodeObject:value forKey:key];
+    }
 }
 @end
